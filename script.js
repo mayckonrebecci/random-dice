dec0348cf0b1cde7d30b4a71dc9d41ae3dced75a
Mayckon Rebecci <mayckon_mmr@hotmail.com>
2021-08-20 23:17:29 -0300

    setting javascript

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let dice1 = document.querySelector(".img1");
 let dice2 = document.querySelector(".img2");
 let btn = document.querySelector(".button");
 
+
 const btnRoll = () => {
     randomNumberDice1 = Math.floor(Math.random() * 6) + 1;
     randomNumberDice2 = Math.floor(Math.random() * 6) + 1;
